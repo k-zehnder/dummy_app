7051079a039ee85071af5aebe90d943540de58c6
Kevin Z <righttrianglesrkewl3@github.com>
2021-10-19 19:23:28 -0700

    api blueprint

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,14 @@
-from flask import current_app
+from flask import jsonify, request, url_for, abort, current_app
 
 from app import db
 from app.models import User, Number
 from app.api import bp
 
-@bp.route('/api/data')
+@bp.route('/api/v1/users')
 def data():
     return {'data': [user.to_dict() for user in User.query]}
+
+@bp.route('/api/v1/users/<int:id>', methods=['GET'])
+def get_user(id):
+    return jsonify(User.query.get_or_404(id).to_dict())
+
